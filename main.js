67ee44b206da7932182d8ae5eeeeb18ebabf0c6d
Sebsterio <sebastian.rosloniec@gmail.com>
2020-01-31 18:43:09 +0100

    instagram intergration

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// --------------- header ----------------
+//*
+// ------------------------ Header ----------------------------
 
 const header = document.querySelector(".main-header");
 
@@ -22,7 +23,7 @@ const scroll = {
 
 window.addEventListener("scroll", () => scroll.debounce());
 
-// ------------ testimonials -------------
+// ---------------------- Testimonials -------------------------
 
 const interval = 5000;
 
@@ -42,4 +43,53 @@ function toggleTestimonial() {
 
 setInterval(toggleTestimonial, interval);
 
-// ------------ / -------------
+// ------------------------- Gallery ----------------------------
+
+const feed = document.querySelector("#ig-feed");
+const toggle = document.querySelector("#toggle-feed");
+
+const fallbackImg = "https://imgpile.com/images/ItvXFr.jpg";
+
+const fallbackPics = (function(length = 12) {
+	return Array.from({ length }, () => fallbackImg);
+})();
+
+function renderPics(pics) {
+	feed.innerHTML = pics
+		.map(
+			item => `
+			<figure class="figure">
+				<img src="${item.media_url || item}" />	
+				<figcaption class="caption">
+					${item.caption || "Error: connection with Instagram failed"}
+					</figcaption>
+				</figure>
+				`
+			//<a href="${item.permalink || item}">IG</a>
+		)
+		.join("");
+}
+
+function loadGallery() {
+	fetch(
+		"https://graph.instagram.com/me/media?fields=caption,id,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=IGQVJWRHVzVkZAVU3VWakhBMUpyTDJYV2lWMUE3aUVQbXFlQkV3SGpBM3B2MEowbktMRDlha0g3YnBxbkZArcFh3QWV4WW1PWWk3OGdDYjEtZAllES1pVU1p5M3NUamh1bFJQVHBiV2Y2aVQ3b2Q3VW9oVjlqNDRVMkNwMHBF"
+	)
+		.then(function(response) {
+			if (response.status === 200)
+				response.json().then(data => renderPics(data.data));
+			else renderPics(fallbackPics);
+		})
+		.catch(function(err) {
+			console.log("Fetch Error :-S", err);
+		});
+}
+
+// expand / collapse gallery
+toggle.addEventListener("click", () => {
+	feed.classList.toggle("open");
+	toggle.classList.toggle("open");
+});
+
+loadGallery();
+
+//*/
--- readme.md
+++ readme.md
@@ -2,6 +2,11 @@
 
 ## TODO
 
-- body { display: grid }; adjust whitespace
+- IG API
 - handle form submit
+- body { display: grid }; adjust whitespace
+- BEM
+- some JS and :hover effects
 - host on Netlify or AWS
+- directory structure
+- section.consultation - superscript color (add .base class?)
