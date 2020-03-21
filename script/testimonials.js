590e1b1a9a72f3da62482193c02a6a6bda93990b
Sebsterio <sebastian.rosloniec@gmail.com>
2020-03-21 18:13:48 +0100

    svg contact link icons; new transformation pix

--- script/testimonials.js
+++ script/testimonials.js
@@ -2,13 +2,18 @@ const interval = 5000;
 
 const testimonials = [...document.querySelectorAll(".testimonial")];
 
+// random number between -range/2 and range/2
+const getRandomNum = range => Math.round((Math.random() - 0.5) * range);
+
 let i = 0;
 let z = 1000;
 function toggleTestimonial() {
-	i = i + 1 < testimonials.length ? i + 1 : 0;
+	i = i + 1 < testimonials.length ? i + 1 : 0; // loop over testimonials
 	const currentPic = testimonials[i];
-	const angle = Math.round((Math.random() - 0.5) * 20);
-	currentPic.style.transform = `rotate(${angle}deg)`;
+	const angle = getRandomNum(20);
+	const offsetX = getRandomNum(20);
+	const offsetY = getRandomNum(20);
+	currentPic.style.transform = `translate(${offsetX}px, ${offsetY}px) rotate(${angle}deg)`;
 	currentPic.classList.add("visible");
 	currentPic.style.zIndex = z;
 	z++;
